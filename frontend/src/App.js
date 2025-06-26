f267725fc4421bde74ca5cd1e9d5b843dbca0849
sirisha-fulara <166629183+sirisha-fulara@users.noreply.github.com>
2025-06-26 12:35:09 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
     setError(null);
 
     try {
-      const response = await fetch("http://localhost:5000/predict", {
+      const response = await fetch("https://quotecraft-1.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ description, skill }),
